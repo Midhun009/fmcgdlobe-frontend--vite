dfb6a86f738b72283961631909df009b98562b72
Midhun009 <mmmidhunvarghese@gmail.com>
2024-10-29 17:59:46 +0530

    removed warrnings

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
 import AboutSection from "../components/About/AboutSection";
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import DashboardNavigation from "../components/Dashboard/DashboardNavigation";
 import DashboardHeader from "../components/Dashboard/DashboardHeader";
